54c0ca1c5fa9b602367ce9d38e1db0f0938776d4
dev-nayeem-53 <devnayeem49@gmail.com>
2025-02-08 13:27:49 +0600

    chore(backend): 🔨  added some changes

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,9 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as compression from 'compression';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/cert.key'),
+    cert: fs.readFileSync('./secrets/cert.crt'),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.use(compression());
+
+  const config = new DocumentBuilder()
+    .setTitle('Coding Challange URL Shortener')
+    .setDescription('TURL Shortener API description')
+    .addTag('url_shortener')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3001);
-  console.log(`app listening on ${await app.getUrl()}`)
+
+  console.log(`app listening on ${await app.getUrl()}`);
 }
 bootstrap();
